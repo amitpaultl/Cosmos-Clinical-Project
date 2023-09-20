f2ec525d366837738a47edd46fbf09637ac4c588
Amit Paul <amitpaultl@gmail.com>
2023-09-20 11:12:00 +0600

    first commit

---
+++ cosmos_clinical_dashboard/assets/js/custom.js
@@ -0,0 +1,71 @@
+(function ($) {
+  "use strict"; // Start of use strict
+
+  // Preloader Start
+  $(window).load(function () {
+    $('#status').fadeOut();
+    $('#preloader')
+        .delay(350)
+        .fadeOut('slow');
+    $('body')
+        .delay(350)
+  });
+// Preloader End
+
+    // Sidebar Menu Start
+     // Toggle the side navigation
+  $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
+    $("body").toggleClass("sidebar-toggled");
+    $(".sidebar").toggleClass("toggled");
+    $('.body-overlay').toggleClass('active');
+    if ($(".sidebar").hasClass("toggled")) {
+      $('.sidebar .collapse').collapse('hide');
+    };
+  });
+
+  // Close any open menu accordions when window is resized below 768px
+  $(window).resize(function() {
+    if ($(window).width() < 768) {
+      $('.sidebar .collapse').collapse('hide');
+    };
+
+  });
+
+  $('.body-overlay').on('click', function() {
+    $(this).removeClass('active');
+    $(".sidebar").removeClass("toggled");
+  });
+    // Sidebar Menu End
+
+
+  // SiebarMenu
+  // Hide submenus
+$('#body-row .collapse').collapse('hide'); 
+
+// Collapse/Expand icon
+$('#collapse-icon').addClass('fa-angle-double-left'); 
+
+// Collapse click
+$('[data-toggle=sidebar-colapse]').click(function() {
+    SidebarCollapse();
+});
+
+function SidebarCollapse () {
+    $('.menu-collapsed').toggleClass('d-none');
+    $('.sidebar-submenu').toggleClass('d-none');
+    $('.submenu-icon').toggleClass('d-none');
+    $('#accordionSidebar').toggleClass('sidebar-expanded sidebar-collapsed');
+    
+    // Treating d-flex/d-none on separators with title
+    var SeparatorTitle = $('.sidebar-separator-title');
+    if ( SeparatorTitle.hasClass('d-flex') ) {
+        SeparatorTitle.removeClass('d-flex');
+    } else {
+        SeparatorTitle.addClass('d-flex');
+    }
+    
+    // Collapse/Expand icon
+    $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
+}
+
+})(jQuery); // End of use strict
